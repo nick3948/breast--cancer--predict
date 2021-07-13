7c31bd7df4c92097883564ec290c1aa4ad88da18
nick3948 <44724470+nick3948@users.noreply.github.com>
2021-07-13 23:59:46 +0530

    Update settings.py

--- cancer/settings.py
+++ cancer/settings.py
@@ -118,5 +118,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='breast.cancer.predict@gmail.com'
-EMAIL_HOST_PASSWORD='ssqopwjvqriylkxw'
+EMAIL_HOST_PASSWORD='BreastCancerPredict@'
 EMAIL_USE_TLS=True
